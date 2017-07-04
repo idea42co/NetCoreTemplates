e2d0cfec2732490fae6be25b4ce37679e09eec6e
Ryan Ward <ryan@idea42.co>
2017-07-04 14:59:59 -0500

    Adding the base of the angular 1x template

---
+++ Content/idea42angular1/gulpfile.js
@@ -0,0 +1,13 @@
+﻿var gulp = require("gulp"),
+    fs = require("fs"),
+    sass = require("gulp-sass");
+
+// other content removed
+
+gulp.task("sass", function () {
+    return gulp.src('Application/Styles/site.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('wwwroot/css'));
+});
+
+gulp.task('default', ['sass']);

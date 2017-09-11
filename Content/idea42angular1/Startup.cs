273ee541b7070e3bb23844c4b39402fce896cc63
Ryan Ward <ryan@idea42.co>
2017-09-10 20:25:20 -0500

    Fixed Angular1.x project for .NET Core 2.0

--- Content/idea42angular1/Startup.cs
+++ Content/idea42angular1/Startup.cs
@@ -43,7 +43,6 @@ namespace test
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
             }
             else
             {

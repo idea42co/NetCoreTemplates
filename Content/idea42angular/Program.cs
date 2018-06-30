be6054061abf4df254548a36ad2ea3cd6ca75c12
Ryan <ryan@idea42.co>
2018-06-30 10:08:39 -0500

    Added new angular template

---
+++ Content/idea42angular/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+
+namespace WebApplicationBasic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:4200")
+                .UseStartup<Startup>()
+                .Build();
+    }
+}

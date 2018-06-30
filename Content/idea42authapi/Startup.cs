0300e708ac044dd92d252a3039a178d572207263
Ryan <ryan@idea42.co>
2018-06-30 14:49:03 -0500

    Adding swagger

--- Content/idea42authapi/Startup.cs
+++ Content/idea42authapi/Startup.cs
@@ -75,6 +75,13 @@ namespace WebApplicationBasic
             }
 
             app.UseAuthentication();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             app.UseMvc();
         }
     }
